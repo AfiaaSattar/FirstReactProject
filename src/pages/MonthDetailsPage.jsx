707d2d4a16195197544f58402cec948db3e7929b
AfiaaSattar <afiasattar2@gmail.com>
2026-05-13 19:52:35 +0300

    13th May 8PM

--- src/pages/MonthDetailsPage.jsx
+++ src/pages/MonthDetailsPage.jsx
@@ -1,6 +1,7 @@
 import { useParams, Link } from 'react-router-dom';
 import { ArrowLeft } from 'lucide-react';
 import { ArrowRight } from 'lucide-react';
+import styled from 'styled-components';
 
  const dayLabels = [
   "Sunday", 
@@ -12,16 +13,44 @@ import { ArrowRight } from 'lucide-react';
   "Saturday"
 ];
 
+  const DateCell = styled.div`
+  color: ${props => props.isEmpty ? 'transparent' : props.isToday ? 'white' : '#444'};
+  background-color: ${props => props.isToday ? '#03a9f4' : 'transparent'};
+  border-radius: ${props => props.isToday ? '20%' : '0'};
+  border-color: ${props => props.isToday ? '#03a9f4' : 'transparent'};
+  &:hover{
+  background-color: ${props => props.isEmpty ? 'transparent' : '#ccc'};
+  border-radius: 8px;
+  }
+  `
+    const getMonthData = (monthIndex) => {
+    const year = 2026;
+    const FirstDay = new Date(year, monthIndex, 1).getDay();
+    const daysInMonth = new Date(year, monthIndex + 1, 0).getDate();
+
+    const blanks = Array(FirstDay).fill(null);
+    const days = Array.from({length: daysInMonth}, (_,i) => (i + 1));
+
+    return [...blanks, ...days];
+  }
 export default function MonthDetailsPage() {
-  const { monthId } = useParams(); // Gets the ID from the URL (e.g., '4')
 
-  // Logic to determine days in the month (for 2026)
-  const getDaysInMonth = (month) => {
-    return new Date(2026, month, 0).getDate();
-  };
+const months = [
+"January", "February", "March", "April", "May", "June", 
+"July", "August", "September", "October", "November", "December"
+];
 
-  const daysCount = getDaysInMonth(monthId);
-  const daysArray = Array.from({ length: daysCount }, (_, i) => i + 1);
+const { monthId } = useParams(); // Gets the ID from the URL (e.g., '4')
+
+const now = new Date();
+const currentMonthIndex = now.getMonth();
+const currentDay = now.getDate();
+const currentYear = now.getFullYear();
+
+
+const monthIdx = parseInt(monthId)-1;
+const calendarDays = getMonthData(monthIdx);
+const currentMonthName = months[monthIdx];
 
   return (
     <div style={{ padding: '40px' }}>
@@ -30,7 +59,7 @@ export default function MonthDetailsPage() {
       gridTemplateColumns: 'repeat(4, 2fr)',
      }}> 
       <Link to="/"> <ArrowLeft /> Back to Dashboard</Link>
-        <ArrowLeft /> Month {monthId} <ArrowRight />
+        <ArrowLeft /> {currentMonthName} <ArrowRight />
 </div>
    <div>
       <div style={{
@@ -51,10 +80,16 @@ export default function MonthDetailsPage() {
         gap: '10px', 
         marginTop: '20px' 
       }}>
-        {daysArray.map((day) => (
+        {calendarDays.map((day,index) => {
+            const isToday =
+            day !== null &&
+             monthIdx === currentMonthIndex &&
+             day === currentDay &&
+             currentYear === 2026;
+         return(
           <Link 
             key={day} 
-            to={`/day/${monthId}/${day}`}
+            to={day ? `/day/${monthId}/${day}` : "#"}
             style={{ 
               padding: '20px', 
               border: '1px solid #ddd', 
@@ -63,9 +98,13 @@ export default function MonthDetailsPage() {
               color: 'black'
             }}
           >
+          <DateCell key={index} isEmpty={!day} isToday={isToday}>
             {day}
-          </Link>
-        ))}
+          </DateCell>
+          </Link>            
+
+            )
+      })}
         </div>
        </div>
       </div>
